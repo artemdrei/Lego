c8dc4d72fd4ce48a2454b703c4bfa0bdc95334a3
Artem Drei <drei.artem@pdffiller.team>
2020-10-04 16:29:58 +0300

    Reinit

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import { Switch, Redirect, Route } from 'react-router-dom';
-
-import Navigation from '@root/containers/Pages/Common/Navigation';
-import Todos from '@root/containers/Pages/Todos';
-import Photos from '@root/containers/Pages/Photos';
 
 import s from './styles.scss';
 
 const App: React.FC = () => {
   return (
     <div className={s.app}>
-      <Navigation />
-      <Switch>
-        <Route path="/todos" component={Todos} />
-        <Route path="/photos" component={Photos} />
-        <Redirect to="/todos" />
-      </Switch>
+      <div className={s.container}>
+        <h1>Level 0</h1>
+        <p>React, TS and nothing more</p>
+      </div>
     </div>
   );
 };
