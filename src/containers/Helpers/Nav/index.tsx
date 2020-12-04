8d600af7475fc60d36bcfcef2f156b98ff8f35c0
Artem Drei <drei.artem@pdffiller.team>
2020-12-04 21:44:03 +0200

    Improve side panel

--- src/containers/Helpers/Nav/index.tsx
+++ src/containers/Helpers/Nav/index.tsx
@@ -8,16 +8,22 @@ import s from 'styled-components';
 const NavStyled = s.nav`
   height: 100vh;
   min-width: 200px;
+  padding: 20px 0;
   border-right: 1px solid var(--faint-weak);
-  padding: 20px 12px;
 `;
 
 const LinkStyled = s.div`
   a {
     display: block;
     width: 100%;
-    margin-bottom: 12px;
+    padding: 8px 20px;
     text-decoration: none;
+    color: var(--font-accent);
+  }
+
+  a:hover {
+    background-color: var(--faint-weak-down);
+    cursor: pointer;
   }
 `;
 
